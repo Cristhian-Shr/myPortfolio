85638e556948411d1855e1a3f23ce4f1bb3e29df
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2023-02-18 10:39:11 -0300

    portfolio profissional

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// efeito digitação
+
+const typing = document.querySelector('[data-js="typing"]');
+const message = ["Olá, eu sou Cristhian Sander Schröer, Desenvolvedor front-end."];
+
+let messageIndex = 0;
+let characterIndex = 0;
+let currentMessage = "";
+let currentCharacters = "";
+
+const type = () => {
+  if (messageIndex === message.length) {
+    messageIndex = 0;
+  }
+
+  currentMessage = message[messageIndex];
+  currentCharacters = currentMessage.slice(0, characterIndex++);
+  typing.textContent = currentCharacters;
+
+  if (currentCharacters.length === currentMessage.length) {
+    messageIndex++;
+    characterIndex = 0;
+  }
+};
+
+setInterval(type, 200);
